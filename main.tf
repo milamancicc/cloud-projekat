28688067bdf634ab903ea531c28c9e33599b5f2a
milamancicc <milamancicc@gmail.com>
2026-06-22 18:07:16 +0200

    promena sku na premium

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_container_registry" "acr" {
   name                = "cloudfinalmila"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
+  sku                 = "Premium"
   admin_enabled       = true
   tags = {
     Environment = "Dev"
